67960a538ed16d83fa6a7e159a6feb0f310f2b88
abdulkadiryilmaz <abdulkadir.yz99@gmail.com>
2026-02-11 16:07:29 +0100

    Python Lessons

---
+++ week1/test.py
@@ -0,0 +1,14 @@
+# fruit = ["A","Apple", "Banana", "Cherry"]
+# # print(fruit[3]) # Output: Cherry   # comand + shift +7 
+# # print(fruit[-1]) # Output: Cherry   
+# # print(fruit[-3]) # Output: Apple
+# # print(fruitprint(fruit[1]) # Output: Apple 
+# # [0:2]) # Output: ['A', 'Apple']
+# # print(fruit[1:4]) # Output: ['Apple', 'Banana', 'Cherry']
+# # print(fruit[:3]) # Output: ['A', 'Apple', 'Banana']
+# # print(fruit[0])
+# fruit.append("Orange")
+# print(fruit) # Output: ['A', 'Apple', 'Banana', 'Cherry
+number = [1, 2, 3, 4, 5]
+number.insert(2, 2.5)
+print(number) 
