b7ad5799ae28be5bcded1e1087300fcf06696d03
abdulkadiryilmaz <abdulkadir.yz99@gmail.com>
2026-02-12 10:24:37 +0100

    class examples

--- week3/classes.py
+++ week3/classes.py
@@ -1,10 +1,21 @@
+# # def printname(name):
+# #     print("Hello " + name)
+# # __name__= "Anoj"
+# # printname(__name__)
+
 # def printname(name):
-#     print("Hello " + name)
-# __name__= "Anoj"
-# printname(__name__)
+#     print("Kadir ")
 
-def printname(name):
-    print("Kadir ")
+# result = printname("Kadir")
+# print(result)
 
-result = printname("Kadir")
-print(result)
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def start(self):
+        print(f"{self.year} {self.brand} {self.model}") 
+my_car = Car("Toyota" , "Corolla")
+my_car.start()
