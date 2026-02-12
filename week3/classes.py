6def90878be261d41152d565a611ddaa950ae2ea
abdulkadiryilmaz <abdulkadir.yz99@gmail.com>
2026-02-12 13:32:20 +0100

    new update

--- week3/classes.py
+++ week3/classes.py
@@ -4,13 +4,15 @@
 # result = printname("Kadir")
 # print(result)
 
-class Car:
-    def __init__(self, brand, model, year):
-        self.brand = brand
-        self.model = model
-        self.year = year
+# class Car:
+#     def __init__(self, brand, model, year):
+#         self.brand = brand
+#         self.model = model
+#         self.year = year
 
-    def start(self):
-        print(f"{self.year} {self.brand} {self.model}") 
-my_car = Car("Toyota", "Corolla", 2020)
-my_car.start()
+#     def start(self):
+#         print(f"{self.year} {self.brand} {self.model}") 
+# my_car = Car("Toyota", "Corolla", 2020)
+# my_car.start()
+
+print("Hello World")    
