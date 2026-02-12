aa0a3b3378f0e1593f3d0739d54765d3365ad7e7
abdulkadiryilmaz <abdulkadir.yz99@gmail.com>
2026-02-12 09:46:15 +0100

    change

---
+++ week3/classes.py
@@ -0,0 +1,10 @@
+# def printname(name):
+#     print("Hello " + name)
+# __name__= "Anoj"
+# printname(__name__)
+
+def printname(name):
+    print("Kadir ")
+
+result = printname("Kadir")
+print(result)
