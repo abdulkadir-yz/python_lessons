20d3c562a40302f36384f12a78787c5d62f6c5f9
abdulkadiryilmaz <abdulkadir.yz99@gmail.com>
2026-02-12 13:07:19 +0100

    update

--- week3/classes.py
+++ week3/classes.py
@@ -10,12 +10,12 @@
 # print(result)
 
 class Car:
-    def __init__(self, brand, model, year):
+    def __init__(self, brand=None, model=None, year=None):
         self.brand = brand
         self.model = model
         self.year = year
 
     def start(self):
         print(f"{self.year} {self.brand} {self.model}") 
-my_car = Car("Toyota" , "Corolla")
+my_car = Car("Toyota" )
 my_car.start()
