87e4c883cf91c2895bf6b8b0ca2167d599c3f47e
abdulkadiryilmaz <abdulkadir.yz99@gmail.com>
2026-02-25 12:47:05 +0100

    Update

--- week5/ex.py
+++ week5/ex.py
@@ -1,1 +1,18 @@
-print("Hello, World!")
+board = ["", "", "", "", "", "", "", "", ""]
+
+def print_board():
+    """Print the current state of the board."""
+    print("\n")
+    print(f" {board[0] or ' '} | {board[1] or ' '} | {board[2] or ' '} ")
+    print("---+---+---")
+    print(f" {board[3] or ' '} | {board[4] or ' '} | {board[5] or ' '} ")
+    print("---+---+---")
+    print(f" {board[6] or ' '} | {board[7] or ' '} | {board[8] or ' '} ")
+    
+    print("\n")
+
+win_lines = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Horizontal
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Vertical
+  [0, 4, 8], [2, 4, 6]              # Diagonal
+]
