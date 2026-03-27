e4ff23c1e06c8104cf7fd23b69f1e72ae076aeb2
abdulkadiryilmaz <abdulkadir.yz99@gmail.com>
2026-03-27 10:05:01 +0100

    update

--- week8/ex_01.py
+++ week8/ex_01.py
@@ -1,12 +1,25 @@
+# my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+# for x in my_list:
+#     print(x)
+
+# for x in my_list:
+#     new_x =float(x * x)
+#     print(new_x)
+
+# for x in my_list:
+#     if x % 6 == 0:
+#         print(x)
+
+# my_dict = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
+
+# for x,y in my_dict.items():
+#     print(f"Key: {x}, Value: {y}")
+
 my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
-for x in my_list:
-    print(x)
-
-for x in my_list:
-    new_x =float(x * x)
-    print(new_x)
-
-for x in my_list:
-    if x % 6 == 0:
-        print(x)
+input_number = int(input("Enter a number to search for: "))
+for number in my_list:
+    if number == input_number:
+        print(f"Found {input_number}!")
+        break
